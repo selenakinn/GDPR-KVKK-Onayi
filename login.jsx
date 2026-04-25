bc5cd2d6c90800559e77a6ec14b62cb49ea44a08
Selen Akın <127742376+selenakinn@users.noreply.github.com>
2026-04-26 00:06:42 +0300

    Add validation and error handling to login form

--- login.jsx
+++ login.jsx
@@ -1,5 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { Form, FormGroup, Label, Input, Button } from 'reactstrap';
+import {
+  Form,
+  FormGroup,
+  Label,
+  Input,
+  Button,
+  FormFeedback,
+} from 'reactstrap';
 import { useHistory } from 'react-router-dom';
 
 import axios from 'axios';
@@ -10,20 +17,68 @@ const initialForm = {
   terms: false,
 };
 
+const errorMessages = {
+  email: 'Please enter a valid email address',
+  password: 'Password must be at least 4 characters long',
+};
+
 export default function Login() {
   const [form, setForm] = useState(initialForm);
-
+  const [isValid, setIsValid] = useState(false);
+  const [errors, setErrors] = useState({
+    email: false,
+    password: false,
+    terms: false,
+  });
   const history = useHistory();
-
+  const validateEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+      );
+  };
+  useEffect(() => {
+    if (
+      validateEmail(form.email) &&
+      form.password.trim().length >= 4 &&
+      form.terms
+    ) {
+      setIsValid(true);
+    } else {
+      setIsValid(false);
+    }
+  }, [form]);
   const handleChange = (event) => {
-    let { name, value, type, checked } = event.target;
-    value = type == 'checkbox' ? checked : value;
+    let { name, value, type } = event.target;
+    value = type === 'checkbox' ? event.target.checked : value;
     setForm({ ...form, [name]: value });
+    if (name == 'email') {
+      if (validateEmail(value)) {
+        setErrors({ ...errors, [name]: false });
+      } else {
+        setErrors({ ...errors, [name]: true });
+      }
+    }
+    if (name == 'password') {
+      if (value.trim().length >= 4) {
+        setErrors({ ...errors, [name]: false });
+      } else {
+        setErrors({ ...errors, [name]: true });
+      }
+    }
+    if (name == 'terms') {
+      if (value) {
+        setErrors({ ...errors, [name]: false });
+      } else {
+        setErrors({ ...errors, [name]: true });
+      }
+    }
   };
 
   const handleSubmit = (event) => {
     event.preventDefault();
-
+    if (!isValid) return;
     axios
       .get('https://6540a96145bedb25bfc247b4.mockapi.io/api/login')
       .then((res) => {
@@ -50,7 +105,9 @@ export default function Login() {
           type="email"
           onChange={handleChange}
           value={form.email}
+          invalid={errors.email}
         />
+        {errors.email && <FormFeedback>{errorMessages.email}</FormFeedback>}
       </FormGroup>
       <FormGroup>
         <Label for="examplePassword">Password</Label>
@@ -61,25 +118,27 @@ export default function Login() {
           type="password"
           onChange={handleChange}
           value={form.password}
+          invalid={errors.password}
         />
+        {errors.password && (
+          <FormFeedback>{errorMessages.password}</FormFeedback>
+        )}
       </FormGroup>
-      {/* reactstrap checkbox ekleyelim*/}
       <FormGroup check>
         <Input
-          type="checkbox"
-          name="terms"
           id="terms"
+          name="terms"
           checked={form.terms}
+          type="checkbox"
           onChange={handleChange}
-        />
-
+          invalid={errors.terms}
+        />{' '}
         <Label htmlFor="terms" check>
           I agree to terms of service and privacy policy
         </Label>
       </FormGroup>
-
       <FormGroup className="text-center p-4">
-        <Button color="primary" disabled={!form.terms}>
+        <Button color="primary" disabled={!isValid}>
           Sign In
         </Button>
       </FormGroup>
